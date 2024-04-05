ef1eca8d8cef289d87015d8e82d4bc2e628a50fa
Joaquín Solla Vázquez <joaquin.solla@udc.es>
2024-04-05 18:11:34 +0200

    Added coords

--- crawler/spiders/crawler.py
+++ crawler/spiders/crawler.py
@@ -56,14 +56,29 @@ class CrawlerSpider(CrawlSpider):
                     name = None
 
                 if maps_url and maps_url.startswith("https://maps.google"):
-                    yield {
-                        "id": self.id_counter,
-                        "name": name,
-                        "info": info,
-                        "quantity": quantity,
-                        "maps_url": maps_url
-                    }
-                    self.id_counter += 1
+
+                    coords = extract_coordinates(maps_url)
+                    if coords is not None:
+
+                        yield {
+                            "id": self.id_counter,
+                            "name": name,
+                            "info": info,
+                            "quantity": quantity,
+                            "maps_url": maps_url,
+                            "coords": coords
+                        }
+                        self.id_counter += 1
 
         except Exception:
             pass
+
+
+def extract_coordinates(url):
+    match = re.search(r'll=(-?\d+\.\d+),(-?\d+\.\d+)', url)
+    if match:
+        lat = match.group(1)
+        lon = match.group(2)
+        return f"{lat}, {lon}"
+    else:
+        return None
